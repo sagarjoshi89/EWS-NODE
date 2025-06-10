f63cab41ae037958955b128b70e87fe8b3449bed
sagarjoshi89 <sagarjoshi89@gmail.com>
2025-06-10 12:40:03 +0530

    Update schemes.js

--- routes/schemes.js
+++ routes/schemes.js
@@ -1,26 +1,31 @@
 const express = require('express');
-const Scheme = require('../models/Scheme');
 const router = express.Router();
+const Scheme = require('../models/Scheme');
+const auth = require('../middleware/auth'); // JWT middleware
 
+// GET all
 router.get('/', async (req, res) => {
-  try {
-    const schemes = await Scheme.find();
-    res.json(schemes);
-  } catch (err) {
-    res.json({ message: 'Server error' });
-  }
+  const schemes = await Scheme.find();
+  res.json(schemes);
 });
 
+// POST new
+router.post('/', auth, async (req, res) => {
+  const scheme = new Scheme(req.body);
+  await scheme.save();
+  res.json(scheme);
+});
 
-router.post('/', async (req, res) => {
-  const { title, description, eligibility, link } = req.body;
-  try {
-    const scheme = new Scheme({ title, description, eligibility, link });
-    await scheme.save();
-    res.json({ message: 'Scheme added' });
-  } catch (err) {
-    res.json({ message: 'Server error' });
-  }
+// PUT update
+router.put('/:id', auth, async (req, res) => {
+  const scheme = await Scheme.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(scheme);
+});
+
+// DELETE
+router.delete('/:id', auth, async (req, res) => {
+  await Scheme.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Deleted' });
 });
 
 module.exports = router;
